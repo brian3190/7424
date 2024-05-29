069b0e27fdc0d3e6d9bde6daede0bf88a325cd54
AlexBorrie <109946019+AlexBorrie@users.noreply.github.com>
2024-05-29 15:34:44 +1200

    Just Some unfinished things

---
+++ app/src/main/java/com/example/assignment_group/Customer.java
@@ -0,0 +1,15 @@
+package com.example.assignment_group;
+
+public class Customer {
+    String firstName;
+    String lastName;
+
+    String userName;
+    String Password;
+
+    long phoneNumber;
+    String email;
+    String address;
+    int bankAccount;
+
+}
