d592a1767ef7c0ccc75f85a0d3eb5dc72f3491ee
AlexBorrie <109946019+AlexBorrie@users.noreply.github.com>
2024-06-07 00:22:11 +1200

    Getting Closer

--- app/src/main/java/com/example/assignment_group/RestaurantAdapter.java
+++ app/src/main/java/com/example/assignment_group/RestaurantAdapter.java
@@ -1,4 +1,51 @@
 package com.example.assignment_group;
 
-public class RestaurantAdapter {
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantHolder> {
+    final private List<Restaurant> restaurantList;
+
+    public RestaurantAdapter(List<Restaurant> restaurantList){
+        this.restaurantList = restaurantList;
+    }
+
+    @NonNull
+    @Override
+    public RestaurantHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+            LayoutInflater lf = LayoutInflater.from(parent.getContext());
+            View v = lf.inflate(R.layout.restaurant_item, parent, false);
+            return new RestaurantHolder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantHolder holder, int position) {
+        Restaurant restaurant = restaurantList.get(position);
+        holder.name.setText(restaurant.getName());
+        holder.address.setText(restaurant.getLocation());
+        holder.category.setText(restaurant.getCategory());
+        holder.phone.setText(restaurant.getPhNumber());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RestaurantDetails.class);
+                intent.putExtra("restaurant", (CharSequence) restaurant);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return restaurantList.size();
+    }
 }
